250a90a1ba8a8d3222c5484d2dda53409f8efc55
Najibmansouur <najibmansour2005@gmail.com>
2024-06-20 17:17:18 +0300

    UPD: nabar has links now

--- src/components/layout/navbar/components/navbar.jsx
+++ src/components/layout/navbar/components/navbar.jsx
@@ -5,6 +5,7 @@ import Search from "./search_bar";
 import MailIconButton from "@/components/icon_buttons/mail_icon_button";
 import { LOGOIMG, MINILOGOIMG } from "@/components/logo/LogoImg";
 import { motion } from "framer-motion";
+import Link from "next/link";
 const Navbar = ({ isBlack = false, scrollY = false, delay, duration }) => {
   // const [color, setColor] = useState(isBlack);
 
@@ -33,8 +34,9 @@ const Navbar = ({ isBlack = false, scrollY = false, delay, duration }) => {
       animate={{ opacity: 1, transition: { delay: delay, duration: duration } }}
       className="sticky top-0 z-30 flex h-20 w-full flex-row items-center justify-center px-8 md:px-16"
     >
-      <MINILOGOIMG className="hidden w-20 active:scale-95 md:ml-2 md:block md:p-2" />
-
+      <Link href="/">
+        <MINILOGOIMG className="hidden w-20 active:scale-95 md:ml-2 md:block md:p-2" />
+      </Link>
       <div className="flex-1">
         <NovoNavigation />
       </div>
