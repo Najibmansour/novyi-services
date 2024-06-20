f24ee0e9a2ba7d0e3968a0cfbf957cbbdfc8e10c
Najibmansouur <najibmansour2005@gmail.com>
2024-06-20 17:16:30 +0300

    ADD: bg_image

--- src/components/layout/bg/bg-gradiant.jsx
+++ src/components/layout/bg/bg-gradiant.jsx
@@ -1,8 +1,14 @@
+import BgImage from "@/assets/bg_image/bg-image";
 import React from "react";
 
 const BgGradiant = () => {
   return (
-    <div className="fixed -z-50 h-screen w-svw bg-radial-gradient bg-fixed"></div>
+    <div className="fixed -z-50 h-screen w-svw">
+      <div className="relative flex h-full w-full items-center justify-center">
+        <BgImage className="absolute z-10 w-[60%] opacity-50" />
+        <div className="absolute h-full w-full bg-radial-gradient bg-fixed" />
+      </div>
+    </div>
   );
 };
 
